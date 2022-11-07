4277ebbe11de5cfcb1c78031ef082b873087435c
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-07 09:02:42 +0300

    ppopulate database with extra data

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,37 @@ user1 =
     password: Faker::Internet.password
   )
 
+user2 =
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+
+user3 =
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+
+user4 =
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+
+user5 =
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+
 puts "seeding cohort ..."
 cohort1 = Cohort.create(name: "cohort one")
+cohort2 = Cohort.create(name: "cohort two")
 
 puts "seeding courses ...."
 course1 =
@@ -26,22 +55,55 @@ course1 =
     description: Faker::Lorem.sentence
   )
 
+course2 =
+  Course.create(
+    name: Faker::Lorem.word,
+    duration: Faker::Number.between(from: 1, to: 12),
+    description: Faker::Lorem.sentence
+  )
+
 puts "seeding lecturers ..."
+
 lecturer1 =
   Lecturer.create(
     user_id: user1.id,
-    course_id: course1.id,
-    cohort_id: cohort1.id
+    course_id: course1.id
+    #cohort_id: cohort1.id
+  )
+
+lecturer2 =
+  Lecturer.create(
+    user_id: user2.id,
+    course_id: course2.id
+    #cohort_id: cohort1.id
   )
 
 puts "seeding students ..."
+
 student1 =
   Student.create(
     user_id: user1.id,
     #course_id: course1.id,
     lecture_id: lecturer1.id,
-    #cohort_id: cohort1.id,
+    cohort_id: cohort1.id,
     grade: 78
   )
 
+student1 =
+  Student.create(
+    user_id: user1.id,
+    #course_id: course1.id,
+    lecture_id: lecturer1.id,
+    cohort_id: cohort2.id,
+    grade: 78
+  )
+
+student1 =
+  Student.create(
+    user_id: user1.id,
+    #course_id: course1.id,
+    lecture_id: lecturer2.id,
+    cohort_id: cohort2.id,
+    grade: 78
+  )
 puts "✅ Done seeding!"
