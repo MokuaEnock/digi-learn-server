b5bd121cabfc31e4220ccb8184bf1e73d0e09949
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-07 04:36:18 +0300

    create models and link them to active record

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,19 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+puts "Destroying current data ..."
+Cohort.destroy_all
+Course.destroy_all
+Lecturer.destroy_all
+Student.destroy_all
+User.destroy_all
+
+puts "Seeding Users ..."
+user1 =
+  User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(8),
+    type: "Student"
+  )
 
 puts "✅ Done seeding!"
